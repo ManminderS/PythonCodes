6e95c2db1f5e07cdabf97aaa378774b827f42c64
Manminder Singh <86635253+ManminderS@users.noreply.github.com>
2023-04-18 14:45:41 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,9 +1,18 @@
 #Step 1 
 import random
-import string
+#print welcome message
 print("Welcome to word guesser Game")
 word_list = ["aardvark", "baboon", "camel", "ironman", "blackpanther", "spiderman"]
+
+#randomise the word and print the word
 random_word = random.choice(word_list)
 print(f"the randomly guessed word is {random_word}")
-user_input = input("Please enter your guess: ")
-print(user_input)
+
+#take input from the player, convert it to lowercase and then split it into
+guess = input("Please enter your guess: ").lower()
+
+for letter in random_word:
+  if letter == guess:
+    print("Input matches an element of the string.")
+  else:
+    print("Input does not match any element of the word.")
