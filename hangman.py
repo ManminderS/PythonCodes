e2c4d4ea68ebc7d39e0510ba1bc12a453ef8c185
Manminder Singh <86635253+ManminderS@users.noreply.github.com>
2023-04-19 14:33:05 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -12,14 +12,17 @@ print(f"random word is {random_word} and has {len(random_word)} characters")
 print()
 #take input from the player, convert it to lowercase and then split it into
 guess = input("Please enter your guess: ").lower()
+length_of_randomword = len(random_word)
 
-for i in range(len(random_word)):
+for i in range(length_of_randomword):
   display.insert(0, "_")
 
 print(display)
 
-for position in range(len(random_word)):
-  letter = random_word[position]
+for position in range(length_of_randomword):
+  letter = random_word[position] #this will allot letter the value of each char one by one as it loops through randomword
   if letter == guess:
     display[position] = letter
+
 print(display)
+  
