9fa18954fce16611ba6e2d47955e56adb6c64a52
Mav <mmsr.aws@gmail.com>
2023-05-08 21:45:46 -0700

    8 May 2023 Function with parameters

---
+++ function_parameter.py
@@ -0,0 +1,12 @@
+def greet():
+  print("You are")
+  print("Killing")
+  print("It")
+
+greet()
+
+def greeting(name,location):
+  print(f"hey {name}")
+  print(f"How is it like to stay in {location}")
+
+greeting(name="Manminder",location= "Vancouver")
